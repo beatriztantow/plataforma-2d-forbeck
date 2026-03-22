6c6eb32bb00e713b37d26c7c34c657f96dbc1384
Beatriz Tantow <beatriz.tantow@gmail.com>
2026-03-22 17:03:15 -0300

    camera

---
+++ scripts/camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var target: Node2D
+
+func _ready() -> void:
+	get_target()
+
+func _process(delta: float) -> void:
+	position = target.position
+
+func get_target():
+	var player_nodes = get_tree().get_nodes_in_group("Player")
+	if player_nodes.size() == 0:
+		push_error("Jogador nao encontrado")
+		return
+	target = player_nodes[0]

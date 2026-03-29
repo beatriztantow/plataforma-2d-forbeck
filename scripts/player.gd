13666f11f980a23749d1a165707798f408c569dd
Beatriz Tantow <beatriz.tantow@gmail.com>
2026-03-29 19:19:45 -0300

    maquina de estados

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,33 +6,81 @@ const SPEED = 80.0
 const JUMP_VELOCITY = -250.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+enum PlayerState {
+	idle, 
+	walk,
+	jump
+}
+var status: PlayerState
+
+func _ready() -> void:
+	go_to_idle_state()
+
+func _physics_process(delta: float) -> void:	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	match status:
+		PlayerState.idle:
+			idle_state()
+		PlayerState.walk:
+			walk_state()
+		PlayerState.jump:
+			jump_state()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	move_and_slide()
+
+func move():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if is_on_floor():
-		if direction > 0:
-			animated_sprite.flip_h = false
-			animated_sprite.play("walk")
-		elif direction < 0:
-			animated_sprite.flip_h = true
-			animated_sprite.play("walk")
-		else: 
-			animated_sprite.play("idle")
-	else:
-		animated_sprite.play("jump")
+	if direction < 0:
+		animated_sprite.flip_h = true
+	elif direction > 0:
+		animated_sprite.flip_h = false
 
-	move_and_slide()
+func go_to_idle_state():
+	status = PlayerState.idle
+	animated_sprite.play("idle")
+
+func go_to_walk_state():
+	status = PlayerState.walk
+	animated_sprite.play("walk")
+
+func go_to_jump_state():
+	status = PlayerState.jump
+	animated_sprite.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+func idle_state():
+	move()
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+func walk_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+			return
+		else:
+			go_to_walk_state()
+			return
